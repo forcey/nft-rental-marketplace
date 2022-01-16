4e4343f037fc6ac2b239d66d8f0325544fd7d229
Todd Li <forcey@gmail.com>
2022-01-15 17:44:53 -0800

    add a bootstrap nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import Login from './components/Login';
+
 function App() {
   return (
-    <div className="App">
-      Ethereum Fullstack Template
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <span class="navbar-brand">NFT Rental Marketplace</span>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/" id="browse-tab">Browse</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/lend" id="lend-tab">Lend</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/return" id="return-tab">Return</a>
+            </li>
+          </ul>
+          <Login/>
+      </nav>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,5 @@
+function Login() {
+    return (<a href="#">Login with Metamask</a>);
+}
+
+export default Login;
