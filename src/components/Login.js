95aa6659679731ea9154ceb5c3713465897298e2
Todd Li <forcey@gmail.com>
2022-01-23 07:15:57 -0800

    display current wallet/network

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,38 @@
-import { Button } from "react-bootstrap";
+import { useRef, useState, useEffect } from "react";
+import { Badge, Button } from "react-bootstrap";
 import web3provider from "../utils/web3provider";
 
 function Login() {
-    const connectWallet = () => web3provider.Enable(true);
+    const didFetchLoginStateRef = useRef(false);
+    const [address, setAddress] = useState();
+    const [network, setNetwork] = useState();
 
-    return (<Button onClick={connectWallet}>Login with Metamask</Button>);
+    useEffect(() => {
+        if (didFetchLoginStateRef.current) { return; }
+        didFetchLoginStateRef.current = true;
+
+        (async () => {
+            if (await web3provider.Enable(false)) {
+                setAddress(await web3provider.signer.getAddress());
+                setNetwork(await web3provider.signer.getChainId());
+            }
+        })();
+    });
+
+    if (address) {
+        const networkMapping = {
+            "1": <Badge pill bg="primary">Mainnet</Badge>,
+            "4": <Badge pill bg="secondary">Rinkeby</Badge>,
+        };
+        const networkLabel = networkMapping[network] ?? <Badge pill bg="warning" text="dark">Other/Local: {network}</Badge>;
+        return (<div style={{textAlign: "right"}}>Wallet: {address} {networkLabel}</div>);
+    } else {
+        const connectWallet = async () => {
+            await web3provider.Enable(true);
+            didFetchLoginStateRef.current = false;
+        };
+        return (<Button onClick={connectWallet}>Login with Metamask</Button>);
+    }
 }
 
 export default Login;
