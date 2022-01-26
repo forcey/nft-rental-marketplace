43c8da729c88c65cbf7dccf8c639db4ee5d72d7c
cal-a-mari <mari.batilando@gmail.com>
2022-01-26 11:45:55 -0800

    Simplify check when rendering card w/ metadata

--- src/routes/return.js
+++ src/routes/return.js
@@ -69,18 +69,11 @@ function ReturnPage() {
                         actionButtonStyle: 'RETURN',
                         didClickActionButton: () => returnNFT(listing),
                     };
-                    if (LoginService.getInstance().chainName === "Unknown") {
-                        // Add placeholder metadata for local network tokens.
-                        card.name = `Katsu #${listing.tokenId.toString()}`;
-                        card.contractName = "Chicken Katsu";
-                        card.imageURI = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E';
-                    } else {
-                        const tokenMetadata = metadata.get(mapKey(card));
-                        if (tokenMetadata) {
-                            card.name = tokenMetadata.name;
-                            card.contractName = tokenMetadata.contractName;
-                            card.imageURI = tokenMetadata.imageURI;
-                        }
+                    const tokenMetadata = metadata.get(mapKey(card));
+                    if (tokenMetadata) {
+                        card.name = tokenMetadata.name;
+                        card.contractName = tokenMetadata.contractName;
+                        card.imageURI = tokenMetadata.imageURI;
                     }
                     return card;
                 });
