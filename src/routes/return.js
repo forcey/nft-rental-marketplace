b22b147c7a542c29d339da542cdc9344561905a1
cal-a-mari <mari.batilando@gmail.com>
2022-01-26 11:35:36 -0800

    Refactor fetch logic for metadata and show fake NFT

--- src/routes/return.js
+++ src/routes/return.js
@@ -4,12 +4,17 @@ import { Alert } from 'react-bootstrap';
 import NFTCardGrid from '../components/NFTCardGrid';
 import LoginService from '../utils/LoginService';
 import {KasuContract, ERC721Contract} from '../utils/abiManager';
+import { FetchMetadata } from "../utils/opensea";
+
+function mapKey(token) {
+    // Normalize the address for case-sensitive map lookup.
+    return `${ethers.utils.getAddress(token.address)}/${token.tokenID}`;
+}
 
 function ReturnPage() {
     const [isLoggedIn, setIsLoggedIn] = useState(LoginService.getInstance().isLoggedIn);
     const [rentedNFTs, setRentedNFTs] = useState([]);
     const returnNFT = async function(listing){
-        console.log('running');
         const tokenContract = ERC721Contract(listing.tokenAddress);
         await tokenContract.approve(listing.lenderAddress, listing.tokenId)
         .then(() =>{
@@ -17,31 +22,71 @@ function ReturnPage() {
                 contract.returnNFT(listing.id)
             })
     }
-    const fetchRentedNFTs = useCallback( async() => {
-        // TODO: Implement fetching returnable NFTs
-        async function getListings(){
-            const contract = KasuContract();
-            const listings = await contract.viewRentedListings();
-            return listings;
-        }
-        const listings = await getListings()
-        setRentedNFTs(listings.map(listing => {
-            const card = {
-                address: listing.tokenAddress,
-                tokenID: listing.tokenId.toString(),
-                listingID: listing.id,
-                name: listing.name,
-                contractName: listing.contractName,
+    const renderFakeNFTs = useCallback(fetchedNFTsWithoutMetadata => {
+        const fakeNFTs = fetchedNFTsWithoutMetadata.map(listing => {
+            return {
+                name: `Katsu #${listing.tokenId.toString()}`,
+                contractName: "Chicken Katsu",
                 imageURI: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+                address: listing.tokenAddress,
+                tokenID: listing.tokenId,
+                listingID: listing.id,
                 collateral: ethers.utils.formatEther(listing.collateralRequired),
                 rentalDuration: listing.duration,
                 interestRate: listing.dailyInterestRate,
                 actionButtonStyle: 'RETURN',
-                //actionButtonDisabled: ethers.utils.getAddress(listing.lenderAddress) === walletAddress,
                 didClickActionButton: () => returnNFT(listing),
-            }
-            return card;
+            };
+        });
+        setRentedNFTs(fakeNFTs);
+    }, [setRentedNFTs]);
+    const fetchRentedNFTs = useCallback(async () => {
+        const contract = KasuContract();
+        const fetchedRentedNFTsWithoutMetadata = await contract.viewRentedListings();
+        console.log('fetchedRentedNFTsWithoutMetadata ', fetchedRentedNFTsWithoutMetadata);
+        const partialMetadata = fetchedRentedNFTsWithoutMetadata.map(listing => ({
+            address: listing.tokenAddress,
+            tokenID: listing.tokenId.toString(),
         }));
+        // Can't fetch metadata on local network.
+        if (LoginService.getInstance().chainName === "Unknown") {
+            renderFakeNFTs(fetchedRentedNFTsWithoutMetadata);
+            return;
+        }
+        FetchMetadata(partialMetadata)
+            .then(response => {
+                const kvPairs = response.map(metadata => [mapKey(metadata), metadata]);
+                const metadata = new Map(kvPairs);
+                console.log('metadata ', metadata);
+                const rentedNFTsWithMetadata = fetchedRentedNFTsWithoutMetadata.map(listing => {
+                    const card = {
+                        address: listing.tokenAddress,
+                        tokenID: listing.tokenId,
+                        listingID: listing.id,
+                        collateral: ethers.utils.formatEther(listing.collateralRequired),
+                        rentalDuration: listing.duration,
+                        interestRate: listing.dailyInterestRate,
+                        actionButtonStyle: 'RETURN',
+                        didClickActionButton: () => returnNFT(listing),
+                    };
+                    if (LoginService.getInstance().chainName === "Unknown") {
+                        // Add placeholder metadata for local network tokens.
+                        card.name = `Katsu #${listing.tokenId.toString()}`;
+                        card.contractName = "Chicken Katsu";
+                        card.imageURI = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E';
+                    } else {
+                        const tokenMetadata = metadata.get(mapKey(card));
+                        if (tokenMetadata) {
+                            card.name = tokenMetadata.name;
+                            card.contractName = tokenMetadata.contractName;
+                            card.imageURI = tokenMetadata.imageURI;
+                        }
+                    }
+                    return card;
+                });
+                setRentedNFTs(rentedNFTsWithMetadata);
+            })
+            .catch(error => console.log(error));
     }, [setRentedNFTs]);
 
     const onLogin = useCallback(() => {
