b42b5026fb9d88901de4f2f306ea9ace5a5ccc67
cal-a-mari <mari.batilando@gmail.com>
2022-01-27 14:47:29 -0800

    Fix typo

--- src/routes/lend.js
+++ src/routes/lend.js
@@ -195,10 +195,10 @@ function LendPage() {
         fetchOwnedOngoingListingsAndRentals();
     }, [fetchOwnedOngoingListingsAndRentals]);
 
-    const closeUnlistingModel = useCallback((didUnlistNFT, listingID) => {
+    const closeUnlistingModal = useCallback((didUnlistNFT, listingID) => {
         setUnlistingModalState({ isShown: false, listingID: '' });
         if (didUnlistNFT) {
-            //Should we be refreshing the listings here if we are already 
+            //Should we be refreshing the listings here if we are already
             //listening for contract events.
             setNFTsListedForLending(nfts => {
                 return nfts.filter(obj => !obj.listingID.eq(listingID))});
@@ -236,8 +236,8 @@ function LendPage() {
             {unlistingModalState.isShown &&
                 <UnlistingModal
                     listingID={unlistingModalState.listingID}
-                    isShown={unlistingModalState.isShown} 
-                    onShouldClose={closeUnlistingModel} />}
+                    isShown={unlistingModalState.isShown}
+                    onShouldClose={closeUnlistingModal} />}
         </Container>
     )
 }
