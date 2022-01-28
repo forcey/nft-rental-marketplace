ab2594c9ccc123898d2112d1f5185c346e848523
Ted Wu <ted.wu@airbnb.com>
2022-01-27 21:51:45 -0800

    refactor local network

--- src/routes/lend.js
+++ src/routes/lend.js
@@ -96,26 +96,34 @@ function LendPage() {
         });
     }, [setNFTsLentOut]);
 
+    const fakeImageURI = 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E';
+    const createBaseNFTCard = useCallback(listing => {
+        return {
+            address: listing.tokenAddress,
+            tokenID: listing.tokenId,
+            listingID: listing.id,
+            collateral: ethers.utils.formatEther(listing.collateralRequired),
+            rentalDuration: listing.duration,
+            interestRate: listing.dailyInterestRate,
+        };
+    })
+
     const fetchOwnedOngoingListingsAndRentals = useCallback(async () => {
         const contract = KasuContract();
-        const fetchedListingsAndRentalsWithoutMetadata = (await contract.viewOwnedOngoingListingsAndRentals());
-        const ongoingListingsWithoutMetadata = fetchedListingsAndRentalsWithoutMetadata.filter(obj => isRentalAvailable(obj));
-        const ongoingRentalsWithoutMetadata = fetchedListingsAndRentalsWithoutMetadata.filter(obj => !isRentalAvailable(obj));
+        const fetchedListingsAndRentals = (await contract.viewOwnedOngoingListingsAndRentals());
+        const ongoingListingsWithoutMetadata = fetchedListingsAndRentals.filter(obj => isRentalAvailable(obj));
+        const ongoingRentalsWithoutMetadata = fetchedListingsAndRentals.filter(obj => !isRentalAvailable(obj));
 
         // Won't fetch metadata on local network.
         if (LoginService.getInstance().chainName === "Unknown") {
             const fakeListedNFTs = ongoingListingsWithoutMetadata.map(obj => {
-                return {
-                    address: obj.tokenAddress,
-                    tokenID: obj.tokenId,
-                    listingID: obj.id,
-                    collateral: ethers.utils.formatEther(obj.collateralRequired),
-                    rentalDuration: obj.duration,
-                    interestRate: obj.dailyInterestRate,
-                    actionButtonStyle: 'UNLIST',
-                    didClickActionButton: () => unlistNFT(obj.id),
-                    imageURI: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-                };
+                const baseNFTCard = createBaseNFTCard(obj);
+
+                baseNFTCard.actionButtonStyle = 'UNLIST';
+                baseNFTCard.didClickActionButton = unlistNFT(obj.id);
+                baseNFTCard.imageURI = fakeImageURI;
+
+                return baseNFTCard;
             });
 
             const fakeLentOutNFTs = ongoingRentalsWithoutMetadata.map(obj => {
@@ -131,19 +139,15 @@ function LendPage() {
                                             .toLocaleDateString("en-US", dateOptions);
                 const isTerminatable = ethers.BigNumber.from(Date.now()).div(1000)
                                         .gte(obj.rental.rentedAt.add(obj.duration * 86400));
-                return {
-                    address: obj.tokenAddress,
-                    tokenID: obj.tokenId,
-                    listingID: obj.id,
-                    collateral: ethers.utils.formatEther(obj.collateralRequired),
-                    rentalDuration: obj.duration,
-                    interestRate: obj.dailyInterestRate,
-                    actionButtonStyle: isTerminatable ? 'TERMINATE_RENTAL' : null,
-                    didClickActionButton: isTerminatable ? terminateRental : null,
-                    rentalDueDate: rentalDueDate,
-                    rentedAtDate: rentedAtDate,
-                    imageURI: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22232%22%20height%3D%22131%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20232%20131%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e704e3109%20text%20%7B%20fill%3A%2380b480%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A12pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e704e3109%22%3E%3Crect%20width%3D%22232%22%20height%3D%22131%22%20fill%3D%22%23a1e1a1%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2284.85546875%22%20y%3D%2270.9%22%3E232x131%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-                };
+
+                const baseNFTCard = createBaseNFTCard(obj);
+
+                baseNFTCard.actionButtonStyle = isTerminatable ? 'TERMINATE_RENTAL' : null;
+                baseNFTCard.didClickActionButton = isTerminatable ? terminateRental : null;
+                baseNFTCard.rentalDueDate = rentalDueDate;
+                baseNFTCard.rentedAtDate = rentedAtDate;
+                baseNFTCard.imageURI = fakeImageURI;
+                return baseNFTCard;
             });
 
             setNFTsListedForLending(fakeListedNFTs);
